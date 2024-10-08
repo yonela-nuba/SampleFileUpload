a19c8e44023a972663344549fd5d9001d4af985b
Yonela Nuba <ynuba@rosebankcollege.co.za>
2024-10-08 21:08:20 +0000

    Added ApplicationDbContext and also created a FileCreation model with all the fields that we will add to our table

---
+++ SampleFileUpload/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SampleFileUpload.Models;
+namespace SampleFileUpload.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) :base(options)
+        {
+                
+        }
+
+        public DbSet<FileCreation> FileCreations { get; set; }
+
+    }
+}
---
+++ SampleFileUpload/Models/FileCreation.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleFileUpload.Models
+{
+    public class FileCreation
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "File Name")]
+        public string FileName { get; set; }
+
+        [Display(Name = "File Type")]
+        public string FileType { get; set; }
+
+        [Display(Name = "File Extension")]
+        public string Extension { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Uploaded By")]
+        public string UploadedBy { get; set; }
+
+        [Display(Name = "Uploaded On")]
+        public DateTime UploadedOn { get; set;}
+    }
+}
