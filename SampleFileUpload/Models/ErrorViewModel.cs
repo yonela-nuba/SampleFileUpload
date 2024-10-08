a4ed4dff33ca67654815be284cb74712700769b9
Yonela Nuba <ynuba@rosebankcollege.co.za>
2024-10-08 18:35:13 +0000

    Add project files.

---
+++ SampleFileUpload/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleFileUpload.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
