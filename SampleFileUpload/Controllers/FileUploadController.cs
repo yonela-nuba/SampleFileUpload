343e414ffc8a8688d3d357a43dc8737f0ef41eb9
Yonela Nuba <ynuba@rosebankcollege.co.za>
2024-10-08 23:09:13 +0000

    Last Changes for the night I am tired

--- SampleFileUpload/Controllers/FileUploadController.cs
+++ SampleFileUpload/Controllers/FileUploadController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SampleFileUpload.Data;
 using SampleFileUpload.Models;
 using SampleFileUpload.ViewModel;
 
@@ -7,10 +8,34 @@ namespace SampleFileUpload.Controllers
     public class FileUploadController : Controller
     {
         //Add ApplicationDbContext here
-
-        public IActionResult Index(FileUploadViewModel vm, IFormFile file)
+        public readonly ApplicationDbContext _context;
+        public FileUploadController(ApplicationDbContext context)
+        {
+                _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index(FileUploadViewModel vm)
         {
             vm.SystemFiles = new List<FileCreation>();
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult UploadFile(FileUploadViewModel vm, IFormFile file)
+        {
+            //var filename = file.FileName;
+            var filename = $"{file.FileName}_{DateTime.Now.ToString("yyyymmddhhmmss")}";
+            var uploadedfile = new FileCreation
+            {
+                UploadedOn = DateTime.Now,
+                UploadedBy = "Yonela Nuba",
+                FileType = file.ContentType,
+                FileName = filename,
+                Description = vm.Description,
+                FilePath = vm.FilePath,
+                //Extension = file.
+
+            };
             return View(vm);
         }
     }
